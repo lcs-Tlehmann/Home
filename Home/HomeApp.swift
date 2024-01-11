b877e89edaec25912dec2a1c22b3c3f3f8972103
Tristan Lehmann <tlehmann@lcs.on.ca>
2024-01-11 14:05:40 -0500

    Initial Commit

---
+++ Home/HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeApp.swift
+//  Home
+//
+//  Created by Tristan Lehmann on 2024-01-11.
+//
+
+import SwiftUI
+
+@main
+struct HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
